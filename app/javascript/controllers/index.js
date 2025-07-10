1925b488c50bd372d0ce8d959cf9fc92ae6c1db9
alexreginelli <108173894+alexreginelli@users.noreply.github.com>
2025-07-10 16:48:43 +0100

    fix deployment

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-
-const application = Application.start()
-const context = require.context(".", true, /\.js$/)
-application.load(definitionsFromContext(context))
